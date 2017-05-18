d9e9c4d3db45bc5529416e510e004cb47f628e68
Student <brbg20-s@dungeon.local>
2017-05-18 15:31:32 +0930

    Working interface

--- app/src/main/java/com/tafe/mcintosh/openweather/WeatherActivity.java
+++ app/src/main/java/com/tafe/mcintosh/openweather/WeatherActivity.java
@@ -20,6 +20,7 @@ import org.json.JSONObject;
 
 import java.sql.Date;
 import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 
 public class WeatherActivity extends AppCompatActivity {
@@ -42,7 +43,9 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-//        weatherFont = Typeface.createFromAsset(getAssets(), "fonts/weather.ttf");
+
+        weatherFont = Typeface.createFromAsset(getAssets(), "fonts/weather.ttf");
+
         updateWeatherData(new CityPreference(this).getCity());
         cityField = (TextView) findViewById(R.id.city_field);
         updatedField = (TextView) findViewById(R.id.updated_field);
@@ -50,8 +53,7 @@ public class WeatherActivity extends AppCompatActivity {
         currentTemperatureField = (TextView) findViewById(R.id.current_temperature_field);
         weatherIcon = (TextView) findViewById(R.id.weather_icon);
 
-
-//        weatherIcon.setTypeface(weatherFont);
+        weatherIcon.setTypeface(weatherFont);
 
 
 
@@ -116,22 +118,22 @@ public class WeatherActivity extends AppCompatActivity {
         try {
             Log.e("json", json.getJSONObject("sys").toString());
             Log.e("json", json.getJSONObject("main").getString("temp"));
-            currentTemperatureField.setText(json.getJSONObject("main").getString("temp")+" degrees");
-//            cityField.setText(json.getString("name").toUpperCase(Locale.US) +
-//                    ", " +
-//                    json.getJSONObject("sys").getString("country"));
+            currentTemperatureField.setText(json.getJSONObject("main").getString("temp")+" ℃");
+            cityField.setText(json.getString("name").toUpperCase(Locale.US) +
+                    ", " +
+                    json.getJSONObject("sys").getString("country"));
 //
-//            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
-//            JSONObject main = json.getJSONObject("main");
+            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = json.getJSONObject("main");
 
-//            detailsField.setText(
-//                    details.getString("description").toUpperCase(Locale.US) +
-//                            "\n" + "Humidity: " + main.getString("humidity") + "%" +
-//                            "\n" + "Pressure: " + main.getString("pressure") + " hPa");
+            detailsField.setText(
+                    details.getString("description").toUpperCase(Locale.US) +
+                            "\n" + "Humidity: " + main.getString("humidity") + "%" +
+                            "\n" + "Pressure: " + main.getString("pressure") + " hPa");
 
-//            currentTemperatureField.setText(
-//                    String.format("%.2f", main.getDouble("temp"))+ " ℃");
-                    //TODO: Find Log output of main
+            currentTemperatureField.setText(
+                    String.format("%.2f", main.getDouble("temp"))+ " ℃");
+                    
                     //http://api.openweathermap.org/data/2.5/weather?q=sydney&units=metric&appid=9f5bae41fceccaf4ff79849fb5455faf
 //                    Log.e("json", json.getJSONObject("main").getString("temp"));
 
@@ -139,9 +141,9 @@ public class WeatherActivity extends AppCompatActivity {
 //            String updatedOn = df.format(new Date(json.getLong("dt")*1000));
 //            updatedField.setText("Last update: " + updatedOn);
 
-//            setWeatherIcon(details.getInt("id"),
-//                    json.getJSONObject("sys").getLong("sunrise") * 1000,
-//                    json.getJSONObject("sys").getLong("sunset") * 1000);
+            setWeatherIcon(details.getInt("id"),
+                    json.getJSONObject("sys").getLong("sunrise") * 1000,
+                    json.getJSONObject("sys").getLong("sunset") * 1000);
 
         }catch(Exception e){
             Log.e("SimpleWeather", "One or more fields not found in the JSON data" + e);
@@ -152,6 +154,7 @@ public class WeatherActivity extends AppCompatActivity {
         int id = actualId / 100;
         String icon = "";
         if(actualId == 800){
+                //TODO get hour of day from Calendar class...
 //            long currentTime = Calendar.get(Calendar.HOUR_OF_DAY);
 //            if(currentTime>=sunrise && currentTime<sunset) {
                 icon = WeatherActivity.this.getString(R.string.weather_sunny);
